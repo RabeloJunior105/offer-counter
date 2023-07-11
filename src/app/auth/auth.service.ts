f4f42b651aacddcec41c0b1b5ffdd84b4ddf3169
Junior Rabelo <rabelojunior105@gmail.com>
2023-07-11 16:47:27 -0300

    feat: add unit test auth validate

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
-    validateApiKey(apikey: string): String {
-        const keys: string[] = process.env.API_KEYS.split(", ")
-        return keys.find(key => apikey == key)
+    validateApiKey(apikey: string): Boolean {
+        const keys: string[] = JSON.parse(`${process.env.API_KEYS}`)
+        return keys.includes(apikey)
     }
 }
