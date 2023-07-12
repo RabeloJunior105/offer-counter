71fe624e9cc0d29165be5dfc6a2f9a8b7ba2e598
Junior Rabelo <rabelojunior105@gmail.com>
2023-07-12 18:18:47 -0300

    Feat:[CURRENCY] create services and repository

--- src/app/currency/currency.controller.ts
+++ src/app/currency/currency.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
+import { CurrencyService } from './currency.service';
+import { ApiKeyAuthGuard } from '../auth/auth.guard';
 
 @Controller('currency')
-export class CurrencyController {}
+export class CurrencyController {
+    constructor(
+        private readonly CurrencyService: CurrencyService
+    ) { }
+
+    @UseGuards(ApiKeyAuthGuard)
+    @Get('/:id')
+    async findId(@Param() { id }) {
+        return await this.CurrencyService.findId(id)
+    }
+}
--- src/app/currency/currency.service.ts
+++ src/app/currency/currency.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { CurrencyRepository } from './currency.repository';
+import { CurrencyEntity } from './currency.entity';
 
 @Injectable()
-export class CurrencyService {}
+export class CurrencyService {
+    constructor(
+        private readonly currencyRepository: CurrencyRepository
+    ) { }
+
+    async findId(id: string): Promise<CurrencyEntity> {
+        return await this.currencyRepository.findId(id)
+    }
+}
