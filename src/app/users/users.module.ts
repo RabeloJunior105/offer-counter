61d9cfd758cfe4502b88f32133d8bfbf44f6542b
Junior Rabelo <rabelojunior105@gmail.com>
2023-07-12 18:19:18 -0300

    Feat:[USERS] create services and repository

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersEntity } from './users.entity';
+import { UsersController } from './users.controller';
+import { UsersRepository } from './users.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UsersEntity])],
-  providers: [UsersService]
+  providers: [UsersService, UsersRepository],
+  controllers: [UsersController]
 })
 export class UsersModule { }
--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { UsersRepository } from './users.repository';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        private readonly UsersRepository: UsersRepository
+    ) { }
+
+    async findId(id: string) {
+        return await this.UsersRepository.findId(id)
+    }
+}
+
